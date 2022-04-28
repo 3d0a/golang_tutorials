5d2d291c63dedcc1a084671dfe1c7d5615ddec51
edoa <korkunov@mail.ru>
2022-04-28 15:10:10 +0300

    return negative 8kyu complete

---
+++ 8kyu/return_negative/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func MakeNegative(x int) int {
+	isNegative := math.Signbit(float64(x))
+    if isNegative {
+		return x
+	}
+	return -1 * x
+    
+}
+
+func main() {
+    fmt.Println(MakeNegative(0))
+}
