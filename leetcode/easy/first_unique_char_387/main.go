406d6a137b03fa585d9a7cfdc260ae574c49313b
3d0a <korkunov.ivan.andr@gmail.com>
2022-06-14 14:57:29 +0300

    completed task for leetcode '383. Ransom Note'

---
+++ leetcode/easy/first_unique_char_387/main.go
@@ -0,0 +1,18 @@
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func firstUniqChar(s string) int {
+	for i, char := range s {
+		if strings.Count(s, string(char)) == 1 {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(firstUniqChar("leetcode"))
+}
