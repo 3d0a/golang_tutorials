f8548bb6bc4dc3049641b86ca2cb04cf828b032e
3d0a <korkunov.ivan.andr@gmail.com>
2022-06-15 09:53:59 +0300

    completed kata '141. Linked List Cycle'

---
+++ leetcode/easy/linked_list_cycle_141/main.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func  hasCycle(head *ListNode) bool {
+  walker, runner := head, head
+  for walker != nil && runner != nil && runner.Next != nil {
+    walker = walker.Next
+    runner = runner.Next.Next
+    if walker == runner {
+      return true
+    }
+  }
+  return false
+}
+
+func main() {
+	tail := ListNode{Val:1, Next: nil}
+	node1 := ListNode{Val:-1, Next:&tail}
+	head := ListNode{Val:4, Next:&node1}
+	tail.Next = &head
+	fmt.Println(hasCycle(&head))
+}
