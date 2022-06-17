6fd51d3a187f275b04932f767b3ea1833fbb6093
3d0a <korkunov.ivan.andr@gmail.com>
2022-06-17 16:49:18 +0300

    completed task '20. Valid Parentheses'

---
+++ leetcode/easy/valid_parentheses_20/main.go
@@ -0,0 +1,45 @@
+package main
+import (
+	"fmt"
+)
+
+type Stack struct {
+	arr []string
+}
+
+func (s *Stack) Push(elem string) {
+	s.arr = append(s.arr, elem)
+}
+
+func (s *Stack) Pop() string {
+	last_elem := s.arr[len(s.arr) - 1]
+	s.arr = s.arr[:len(s.arr) - 1]
+	return last_elem
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(s.arr) == 0
+}
+
+func isValid(s string) bool {
+	stack := Stack{}
+	for _, elem := range s {
+		strElem := string(elem)
+		switch {
+			case strElem == "{":
+				stack.Push("}")
+			case strElem == "(":
+				stack.Push(")")
+			case strElem ==  "[":
+				stack.Push("]")
+			case stack.IsEmpty() || stack.Pop() != strElem :
+				return false
+		}
+	}
+	return stack.IsEmpty()
+}
+
+func main() {
+	str := "{}()[]"
+	fmt.Println(isValid(str))
+}
