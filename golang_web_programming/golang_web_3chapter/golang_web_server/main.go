16fbba72d64f596febdae5ff74049b75ce66f153
3d0a <korkunov.ivan.andr@gmail.com>
2022-08-08 13:06:02 +0300

    deleted handlers and used functions as handlers

--- golang_web_programming/golang_web_3chapter/golang_web_server/main.go
+++ golang_web_programming/golang_web_3chapter/golang_web_server/main.go
@@ -4,25 +4,20 @@ import (
 	"net/http"
 )
 
-type HelloHandler struct{}
 
-func (h *HelloHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+func helloHandler (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+func worldHandler (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "World!")
 }
 
 func main() {
-	hello := HelloHandler{}
-	world := WorldHandler{}
 	server := http.Server{
 		Addr: ":8080",
 	}
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/world", worldHandler)
 	server.ListenAndServe()
 }
